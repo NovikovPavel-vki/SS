9af1027a99cedf6b01a67773c1f845d90e807331
NovikovPavel-vki <Dotanam@mail.ru>
2021-06-16 05:15:19 +0700

    SS

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LibSystemNumbers;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number system: ");
+            int firstBase = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number: ");
+            string stringNumbers = Console.ReadLine();
+
+            Console.Write("Enter second number system: ");
+            int secondBase = Convert.ToInt32(Console.ReadLine());
+
+            char[] result = Number.TranslateToSystemNumbers(stringNumbers, firstBase, secondBase);
+
+            Console.WriteLine(result);
+        }
+    }
+}
